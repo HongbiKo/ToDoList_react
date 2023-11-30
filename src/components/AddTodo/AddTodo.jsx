7ac16f29244bac84045a8d527bdb3eaf5a8119ed
HongbiKo <rhghdql666@gmail.com>
2023-11-30 17:58:36 +0900

    styling TodoList.module.css & AddTodo.module.css

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
+import styles from './AddTodo.module.css'
 
 export default function AddTodo({onAdd}) {
   const [text, setText] = useState("");
@@ -16,9 +17,9 @@ export default function AddTodo({onAdd}) {
     setText('');
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="todo" placeholder="Add Todo" value={text} onChange={handleChange}></input>
-      <button>Add</button>
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <input className={styles.input}type="text" name="todo" placeholder="Add Todo" value={text} onChange={handleChange}></input>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
