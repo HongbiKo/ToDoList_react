f296b572c7e2e00a22a4e30492b7102bfc354cbc
HongbiKo <rhghdql666@gmail.com>
2023-12-01 17:25:28 +0900

    fixing bug(input & label matching)

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,7 +3,7 @@ import { FaTrashAlt } from "react-icons/fa";
 import styles from './Todo.module.css'
 
 export default function Todo({todo, onUpdate, onDelete}) {
-  const {text, status} = todo;
+  const {id, text, status} = todo;
   const handleChange = (e) => {
     const status = e.target.checked ? 'completed' : 'active';
     onUpdate({...todo, status: status});
@@ -12,9 +12,9 @@ export default function Todo({todo, onUpdate, onDelete}) {
 
   return (
     <li className={styles.todo}>
-      <input className={styles.checkbox}type="checkbox" checked={status === "completed"} onChange={handleChange}/>
-      <label className={styles.text}>{text}</label>
-      <span className={styles.icon}><button className={styles.button}onClick={handleClick}><FaTrashAlt /></button></span> 
+      <input className={styles.checkbox} type="checkbox" id={id} checked={status === "completed"} onChange={handleChange}/>
+      <label className={styles.text} htmlFor={id}>{text}</label>
+      <span className={styles.icon}><button className={styles.button} onClick={handleClick}><FaTrashAlt /></button></span> 
     </li>
   );
 }
